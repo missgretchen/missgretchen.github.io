6a7cea8c755f4cff399bdbebbafac02540478c08
Gretchen Eggers <gretchen.eggers@gmail.com>
2024-11-03 19:02:32 -0300

    refactor to use vite, add antd, remove unneeded dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <span className="material-symbols-rounded App-logo">construction</span>
         <p>
           Hi there! I'm still building my website. Please check back later.
         </p>
